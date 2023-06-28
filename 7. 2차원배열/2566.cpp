e05df7cdee9179ddc6534aefb64fdb07172f4363
Musk <yuiop1029@naver.com>
2023-06-28 21:59:57 +0900

    C++

---
+++ 7. 2차원배열/2566.cpp
@@ -0,0 +1,25 @@
+#include<algorithm>
+#include<string>
+#include<iostream>
+using namespace std;
+
+int main() {
+    int max=0, x, y;
+
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            int temp;
+            cin >> temp;
+
+            if(temp>max){
+                max = temp;
+                x = i;
+                y = j;
+            }
+        }
+    }
+
+    cout << max << endl << x << " " << y;
+
+
+}
